bdff8a146cc9f495929fe5161a9e6e7ce0681dc0
Bossmaleo1 <bossprogrammaleo@gmail.com>
2024-01-09 04:14:48 +0100

    Initial Commit

---
+++ Shuzi/ShuziApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShuziApp.swift
+//  Shuzi
+//
+//  Created by Sidney MALEO on 09/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShuziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
